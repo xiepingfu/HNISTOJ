7d634d2d5de8da8ef7854207bbb323690a4b5b3e
xiepingfu <lightxpf@gmail.com>
2019-03-04 21:17:48 +0800

    reception user edit

---
+++ modules/api_reception.js
@@ -0,0 +1,66 @@
+let User = syzoj.model('user');
+let Problem = syzoj.model('problem');
+let File = syzoj.model('file');
+const Email = require('../libs/email');
+const jwt = require('jsonwebtoken');
+
+// Reception Sign up
+app.post('/api/reception_sign_up', async (req, res) => {
+    try {
+      res.setHeader('Content-Type', 'application/json');
+      let user = await User.fromName(req.body.username);
+      if (user) throw 2008;
+  
+  
+      // Because the salt is "syzoj2_xxx" and the "syzoj2_xxx" 's md5 is"59cb..."
+      // the empty password 's md5 will equal "59cb.."
+      let syzoj2_xxx_md5 = '59cb65ba6f9ad18de0dcd12d5ae11bd2';
+      if (req.body.password === syzoj2_xxx_md5) throw 2007;
+      if (!syzoj.utils.isValidUsername(req.body.username)) throw 2002;
+      if (req.body.realname == '') throw 3002;
+      
+  
+      if (syzoj.config.register_mail) {
+        let sendObj = {
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+        };
+  
+        const token = jwt.sign(sendObj, syzoj.config.email_jwt_secret, {
+          subject: 'register',
+          expiresIn: '2d'
+        });
+  
+        const vurl = req.protocol + '://' + req.get('host') + syzoj.utils.makeUrl(['api', 'sign_up_confirm'], { token: token });
+        try {
+          await Email.send(req.body.email,
+            `${req.body.username} 的 ${syzoj.config.title} 注册验证邮件`,
+            `<p>请点击该链接完成您在 ${syzoj.config.title} 的注册：</p><p><a href="${vurl}">${vurl}</a></p><p>如果您不是 ${req.body.username}，请忽略此邮件。</p>`
+          );
+        } catch (e) {
+          return res.send({
+            error_code: 2010,
+            message: require('util').inspect(e)
+          });
+        }
+  
+        res.send(JSON.stringify({ error_code: 2 }));
+      } else {
+        user = await User.create({
+          username: req.body.username,
+          password: req.body.password,
+          realname: req.body.realname,
+          phone: req.body.phone,
+          public_email: true
+        });
+        await user.save();
+  
+        res.send(JSON.stringify({ error_code: 1 }));
+      }
+    } catch (e) {
+      syzoj.log(e);
+      res.send(JSON.stringify({ error_code: e }));
+    }
+});
+  
--- modules/reception.js
+++ modules/reception.js
@@ -63,3 +63,79 @@ app.get('/reception/register', async (req, res) => {
         })
       }
 });
+
+
+app.get('/reception/:id/edit', async (req, res) => {
+  try {
+    let id = parseInt(req.params.id);
+    let user = await User.fromID(id);
+    if (!user) throw new ErrorMessage('无此用户。');
+
+    let allowedEdit = await user.isAllowedEditBy(res.locals.user);
+    if (!allowedEdit) {
+      throw new ErrorMessage('您没有权限进行此操作。');
+    }
+
+    user.privileges = await user.getPrivileges();
+
+    res.locals.user.allowedManage = await res.locals.user.hasPrivilege('manage_user');
+
+    res.render('user_edit', {
+      edited_user: user,
+      error_info: null
+    });
+  } catch (e) {
+      syzoj.log(e);
+      res.render('error', {
+        err: e
+      });
+    }
+  });
+
+  app.get('/forget', async (req, res) => {
+  res.render('forget');
+});
+
+app.post('/reception/:id/edit', async (req, res) => {
+  let user;
+  try {
+    let id = parseInt(req.params.id);
+    user = await User.fromID(id);
+    if (!user) throw new ErrorMessage('无此用户。');
+
+    let allowedEdit = await user.isAllowedEditBy(res.locals.user);
+    if (!allowedEdit) throw new ErrorMessage('您没有权限进行此操作。');
+
+    if (req.body.old_password && req.body.new_password) {
+      if (user.password !== req.body.old_password && !await res.locals.user.hasPrivilege('manage_user')) throw new ErrorMessage('旧密码错误。');
+      user.password = req.body.new_password;
+    }
+
+    if (res.locals.user && await res.locals.user.hasPrivilege('manage_user')) {
+      if (!syzoj.utils.isValidUsername(req.body.username)) throw new ErrorMessage('无效的用户名。');
+      user.username = req.body.username;
+      user.email = req.body.email;
+      user.phone = req.phone;
+    }
+
+    await user.save();
+
+    if (user.id === res.locals.user.id) res.locals.user = user;
+
+    user.privileges = await user.getPrivileges();
+    res.locals.user.allowedManage = await res.locals.user.hasPrivilege('manage_user');
+
+    res.render('reception_user_edit', {
+      edited_user: user,
+      error_info: ''
+    });
+  } catch (e) {
+    user.privileges = await user.getPrivileges();
+    res.locals.user.allowedManage = await res.locals.user.hasPrivilege('manage_user');
+
+    res.render('reception_user_edit', {
+      edited_user: user,
+      error_info: e.message
+    });
+  }
+});
